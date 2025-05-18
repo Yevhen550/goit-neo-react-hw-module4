8832c837c0a07bcef0873f8e51dedc597816f419
Yevhen <benatanlev@icloud.com>
2025-05-18 14:49:49 +0300

    Update LoadMoreBtn.jsx

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,9 +1,9 @@
 import s from "./LoadMoreBtn.module.css";
 
-const LoadMoreBtn = () => {
+const LoadMoreBtn = ({ onClick }) => {
   return (
     <div className={s.loadMoreBtn}>
-      <button className={s.btn} type="button">
+      <button className={s.btn} type="button" onClick={onClick}>
         Load more
       </button>
     </div>
